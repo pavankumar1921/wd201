078b9f6e20f89ce43f13358e1d67e58dfa0ff631
Pavan Kumar <pavankittu19@gmail.com>
2023-03-24 22:19:14 +0530

    changed

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,9 +3,35 @@ const hello = () => {
 };
 hello();
 
-const fs = require("fs")
-fs.writeFile("sample.txt","Welcome to Node.js fs module.",
-(err)=>{
-    if(err) throw err;
-    console.log("file created")
-})
+// const http = require("http")
+// const fs = require("fs")
+
+
+
+// const server = http.createServer((req,res)=>{
+//    const stream = fs.createReadStream("sample.txt");
+//    stream.pipe()
+//     // fs.readFile("sample.txt",(err,data)=>{
+//     //     res.end(data)
+//     // })
+// })
+// server.listen(4000)
+
+
+// fs.readFile("sample.txt",(err,data)=>{
+//     if (err) throw err;
+//     console.log(data.toString())
+// })
+
+// fs.appendFile("sample.txt","after updating file",(err)=>{
+//     if(err) throw err;
+//     console.log("file appended")
+// })
+// fs.rename("sample.txt","test.txt",(err)=>{
+//     if(err) throw err;
+//     console.log("changed file name")
+// })
+// fs.unlink("test.txt",(err)=>{
+//     if(err) throw err;
+//     console.log("file deleted")
+// })
