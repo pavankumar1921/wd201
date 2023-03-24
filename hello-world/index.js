db9d5e86db999edba06426d911f79a4cce536626
Pavan Kumar <pavankittu19@gmail.com>
2023-03-24 20:52:59 +0530

    fs

--- hello-world/index.js
+++ hello-world/index.js
@@ -2,3 +2,10 @@ const hello = () => {
     console.log("Hello GitHub!");
 };
 hello();
+
+const fs = require("fs")
+fs.writeFile("sample.txt","Welcome to Node.js fs module.",
+(err)=>{
+    if(err) throw err;
+    console.log("file created")
+})
