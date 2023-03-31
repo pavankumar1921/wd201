c44ee2b9a0fef52aac3bbd38ca37fa98a69d9632
Pavan Kumar <pavankittu19@gmail.com>
2023-03-31 20:25:38 +0530

    completed

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,30 +1,46 @@
-const todoList = require('../todo')
-const {all,markAsComplete,add} = todoList()
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-describe("todolist test suite",() =>{
-    beforeAll(()=>{
-        add(
-            {
-                title:"Buying earphones",
-                completed:false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        ) 
+describe('todolist test suite', () => {
+  beforeAll(() => {
+    const today = new Date();
+      add({
+        title: "Buying earphones",
+        completed: false,
+        dueDate: new Date(today.getTime() - 86400000).toISOString().slice(0,10)
+      }),
+      add({
+        title: "buying mobile",
+        completed: false,
+        dueDate: new Date().toISOString().slice(0,10)
+      }),
+      add({
+        title: "buying connector",
+        completed: false,
+        dueDate: new Date(today.getTime() + 86400000).toISOString().slice(0,10)
     })
-    test("adding todo list",()=>{
-        const todoItemsCount = all.length
-        add(
-            {
-                title:"Buying earphones",
-                completed:false,
-                dueDate: new Date().toLocaleDateString("en-CA")
-            }
-        )
-       expect(all.length).toBe(todoItemsCount+1) 
-    })
-    test("Marking as complete",()=>{
-        expect(all[0].completed).toBe(false)
-        markAsComplete(0)
-        expect(all[0].completed).toBe(true)
-    })
-})
+  });
+  test('adding new todo', () => {
+    const todoItemsCount = all.length;
+    add({
+      title: 'Buying charger',
+      completed: false,
+      dueDate: new Date().toISOString().slice(0,10)
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test('Marking as complete', () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test('Checking overdue', () => {
+    expect(overdue().length).toBe(1)
+  })
+  test('Checking dueToday', () => {
+    expect(dueToday().length).toBe(2)
+  })
+  test('checking dueLAter', () => {
+    expect(dueLater().length).toBe(1)
+  })
+});
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,37 +1,44 @@
 const todoList = () => {
-    all = []
-    const add = (todoItem) => {
-      all.push(todoItem)
-    }
-    const markAsComplete = (index) => {
-      all[index].completed = true
-    }
-  
-    const overdue = () => {
-      return all.filter((dt)=>dt.dueDate<today)
-    }
-  
-    const dueToday = () => {
-      return all.filter((dt)=>dt.dueDate==today)
-    }
-  
-    const dueLater = () => {
-      return all.filter((dt)=>dt.dueDate>today)
-    }
-  
-    const toDisplayableList = (list) => {
-      return list.map((dt)=>`${dt.completed ? "[x]":"[ ]"} ${dt.title} ${dt.dueDate == today ? "":dt.dueDate}`).join("\n")
-    }
-  
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList
-    };
-  };
- 
-  module.exports = todoList;
+  const all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true
+  }
+
+  const overdue = () => {
+    return all.filter((dt) => dt.dueDate < new Date().toISOString().slice(0, 10))
+  }
+
+  const dueToday = () => {
+    return all.filter((dt) => dt.dueDate === new Date().toISOString().slice(0, 10))
+  }
+
+  const dueLater = () => {
+    return all.filter((dt) => dt.dueDate > new Date().toISOString().slice(0, 10))
+  }
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (dt) =>
+          `${dt.completed ? '[x]' : '[ ]'} ${dt.title} ${
+            dt.dueDate === new Date().toISOString().slice(0, 10) ? '' : dt.dueDate
+          }`.trim()
+      )
+      .join('\n')
+  }
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList
+  }
+}
+
+module.exports = todoList
