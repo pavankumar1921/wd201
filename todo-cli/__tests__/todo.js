04470377e1c46e33b9067efdfae8c44fbb424365
Pavan Kumar <pavankittu19@gmail.com>
2023-03-31 20:31:44 +0530

    Added lint staged

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,46 +1,50 @@
+/* eslint-disable no-undef */
+
 const todoList = require("../todo");
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-describe('todolist test suite', () => {
+describe("todolist test suite", () => {
   beforeAll(() => {
     const today = new Date();
-      add({
-        title: "Buying earphones",
-        completed: false,
-        dueDate: new Date(today.getTime() - 86400000).toISOString().slice(0,10)
-      }),
+    add({
+      title: "Buying earphones",
+      completed: false,
+      dueDate: new Date(today.getTime() - 86400000).toISOString().slice(0, 10),
+    }),
       add({
         title: "buying mobile",
         completed: false,
-        dueDate: new Date().toISOString().slice(0,10)
+        dueDate: new Date().toISOString().slice(0, 10),
       }),
       add({
         title: "buying connector",
         completed: false,
-        dueDate: new Date(today.getTime() + 86400000).toISOString().slice(0,10)
-    })
+        dueDate: new Date(today.getTime() + 86400000)
+          .toISOString()
+          .slice(0, 10),
+      });
   });
-  test('adding new todo', () => {
+  test("adding new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: 'Buying charger',
+      title: "Buying charger",
       completed: false,
-      dueDate: new Date().toISOString().slice(0,10)
+      dueDate: new Date().toISOString().slice(0, 10),
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
-  test('Marking as complete', () => {
+  test("Marking as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test('Checking overdue', () => {
-    expect(overdue().length).toBe(1)
-  })
-  test('Checking dueToday', () => {
-    expect(dueToday().length).toBe(2)
-  })
-  test('checking dueLAter', () => {
-    expect(dueLater().length).toBe(1)
-  })
+  test("Checking overdue", () => {
+    expect(overdue().length).toBe(1);
+  });
+  test("Checking dueToday", () => {
+    expect(dueToday().length).toBe(2);
+  });
+  test("checking dueLAter", () => {
+    expect(dueLater().length).toBe(1);
+  });
 });
