8ca620b543cfbe7684400c49547d6797f52e6a61
Pavan Kumar <pavankittu19@gmail.com>
2023-04-25 23:38:46 +0530

    added user table

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
-app.listen(4000, () => {
-  console.log("Started express server at port 3000");
+// app.listen(4000, () => {
+//   console.log("Started express server at port 3000");
+// });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server started at port 3000");
 });
